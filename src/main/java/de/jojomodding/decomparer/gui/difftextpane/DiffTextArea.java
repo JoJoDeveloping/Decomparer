467e866268567bc8091ed8aea433a2d90a70c68a
JoJoDeveloping <jojohostert@gmail.com>
2019-05-05 22:24:30 +0200

    Add lines
    Signed-off-by: JoJoDeveloping <jojohostert@gmail.com>

--- src/main/java/de/jojomodding/decomparer/gui/difftextpane/DiffTextArea.java
+++ src/main/java/de/jojomodding/decomparer/gui/difftextpane/DiffTextArea.java
@@ -4,12 +4,13 @@ package de.jojomodding.decomparer.gui.difftextpane;
 import com.github.difflib.DiffUtils;
 import com.github.difflib.patch.Patch;
 import com.github.difflib.patch.PatchFailedException;
-import de.jojomodding.decomparer.processing.DiffFormatter;
+import de.jojomodding.decomparer.processing.FormattedDiff;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -21,7 +22,8 @@ public class DiffTextArea extends JComponent implements ComponentListener {
     private int leftOffset;
 
     private List<ColorizedString> left, right;
-    private int textlenmaxleft, texlenmaxright, textlenmax;
+    private int textlenmaxleft, texlenmaxright, textlenmax, xStart;
+    private BitSet linesWithChange;
 
     public DiffTextArea(JScrollBar bar, String fontName){
         this.bar = bar;
@@ -39,12 +41,14 @@ public class DiffTextArea extends JComponent implements ComponentListener {
 
     public void setText(List<String> text, Patch<String> patch) {
         processText(text, patch);
+        String mln = Integer.toString(left.size());
+        xStart = getFontMetrics(getFont()).stringWidth(mln) + 7;
         this.textlenmaxleft = left.stream().map(s -> getFontMetrics(getFont()).stringWidth(s.getBase())).max(Integer::compareTo).orElse(0);
         this.texlenmaxright = right.stream().map(s -> getFontMetrics(getFont()).stringWidth(s.getBase())).max(Integer::compareTo).orElse(0);
         textlenmax = Math.max(textlenmaxleft, texlenmaxright);
         bar.setMaximum(textlenmax);
         bar.setVisibleAmount(getWidth()/2-10);
-        bar.setVisible(getWidth() < texlenmaxright+textlenmaxleft+18);
+        bar.setVisible(getWidth() < texlenmaxright+textlenmaxleft+xStart+18);
         bar.setValue(0);
         setMinimumSize(new Dimension(200, getFontMetrics(getFont()).getHeight()*text.size()+4));
         setPreferredSize(getMinimumSize());
@@ -58,20 +62,36 @@ public class DiffTextArea extends JComponent implements ComponentListener {
         g.setColor(getBackground());
         g.fillRect(0,0, getWidth(), getHeight());
         g.setColor(Color.BLACK);
-        g.fillRect(getWidth()/2-5,0,10,getHeight());
+        int width = getWidth()-xStart;
+        int newOffset = width/2+5;
+        g.fillRect(xStart+newOffset-10,0,10,getHeight());
+        g.fillRect(xStart-2, 0, 2, getHeight());
         g.setColor(getForeground());
-        int newOffset = getWidth()/2+5;
-        g.clipRect(2,2,newOffset-14,getHeight()-4);
+        g.clipRect(xStart+2,2,newOffset-14,getHeight()-4);
         int y = 2+getFontMetrics(getFont()).getAscent(), dy = getFontMetrics(getFont()).getHeight();
         for(ColorizedString s : left){
-            drawColoredString(s, -leftOffset+2,y, g);
+            drawColoredString(s, xStart-leftOffset+2,y, g);
             y+=dy;
         }
         g.setClip(clip);
-        g.clipRect(newOffset+2,2,newOffset-14,getHeight()-4);
+        g.clipRect(xStart+newOffset+2,2,newOffset-14,getHeight()-4);
         y = 2+getFontMetrics(getFont()).getAscent();
         for(ColorizedString s : right){
-            drawColoredString(s, newOffset-leftOffset+2,y,g);
+            drawColoredString(s, xStart+newOffset-leftOffset+2,y,g);
+            y+=dy;
+        }
+        g.setClip(clip);
+        g.clipRect(2, 2, xStart-5, getHeight());
+        y = 2+getFontMetrics(getFont()).getAscent();
+        String format = "%1$"+(Integer.toString(left.size()).length())+"d";
+        for(int i = 0; i < left.size(); i++){
+            boolean b = linesWithChange.get(i);
+            if(b){
+                g.setColor(new Color(64,64,128));
+                g.fillRect(0,y- g.getFontMetrics().getAscent(), xStart-2, g.getFontMetrics().getHeight());
+            }
+            g.setColor(Color.WHITE);
+            g.drawString(String.format(format, 1+i),2,y);
             y+=dy;
         }
     }
@@ -80,7 +100,7 @@ public class DiffTextArea extends JComponent implements ComponentListener {
     public void componentResized(ComponentEvent componentEvent) {
         bar.setMaximum(textlenmax);
         bar.setVisibleAmount(getWidth()/2-10);
-        bar.setVisible(getWidth() < texlenmaxright+textlenmaxleft+18);
+        bar.setVisible(getWidth() < texlenmaxright+textlenmaxleft+xStart+18);
         repaint();
     }
 
@@ -118,8 +138,13 @@ public class DiffTextArea extends JComponent implements ComponentListener {
     }
 
     private void processText(List<String> text, Patch<String> patch){
-        Map.Entry<List<ColorizedString>, List<ColorizedString>> i = DiffFormatter.format(text, patch);
-        this.left = i.getKey();
-        this.right = i.getValue();
+        FormattedDiff fd = new FormattedDiff(text, patch);
+        this.left = fd.getLeft();
+        this.right = fd.getRight();
+        this.linesWithChange =fd.getChangedLines();
+        while(left.size() < right.size())
+            left.add(new ColorizedString("", Color.white, Color.black));
+        while(right.size() < left.size())
+            right.add(new ColorizedString("", Color.white, Color.black));
     }
 }
--- src/main/java/de/jojomodding/decomparer/processing/FormattedDiff.java
+++ src/main/java/de/jojomodding/decomparer/processing/FormattedDiff.java
@@ -7,37 +7,56 @@ import com.github.difflib.text.DiffRowGenerator;
 import de.jojomodding.decomparer.gui.difftextpane.ColorizedString;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
-public class DiffFormatter {
+public class FormattedDiff {
 
-    public static Map.Entry<List<ColorizedString>, List<ColorizedString>> format(List<String> left, Patch<String> right){
+    private List<ColorizedString> left, right;
+    private BitSet changedLines;
+
+    public FormattedDiff(List<String> left, Patch<String> right){
+        this.left = new ArrayList<>(left.size());
+        this.right = new ArrayList<>(left.size());
+        changedLines = new BitSet();
         if(right != null)try {
-            List<ColorizedString> resl = new ArrayList<>(left.size()), resr = new ArrayList<>(left.size());
             List<DiffRow> dr = DiffRowGenerator.create().showInlineDiffs(true).oldTag(f -> "\0").newTag(f -> "\0").reportLinesUnchanged(true).build().generateDiffRows(left, right);
-            for(DiffRow r : dr){
+            for(int line = 0; line < dr.size(); line++){
+                DiffRow r = dr.get(line);
                 String[] leftSubs = r.getOldLine().split("\0", -1);
                 ColorizedString cs = new ColorizedString(leftSubs[0], Color.white, new Color(64,64,64));
                 for(int i = 1; i < leftSubs.length; i++){
                     cs.append(leftSubs[i], Color.white, new Color((i%2)==0?64:128,64,64));
+                    changedLines.set(line);
                 }
-                resl.add(cs);
+                this.left.add(cs);
                 String[] rightSubs = r.getNewLine().split("\0", -1);
                 cs = new ColorizedString(rightSubs[0], Color.white, new Color(64,64,64));
                 for(int i = 1; i < rightSubs.length; i++){
                     cs.append(rightSubs[i], Color.white, new Color(64,(i%2)==0?64:128,64));
+                    changedLines.set(line);
                 }
-                resr.add(cs);
+                this.right.add(cs);
             }
-            return Map.entry(resl, resr);
+            return;
         } catch (DiffException e) {
             e.printStackTrace();
         }
-        return Map.entry(List.of(new ColorizedString("Could not compute diff!", Color.white, Color.red)), List.of());
+        this.left = List.of(new ColorizedString("Could not compute diff!", Color.white, Color.red));
+        this.right = List.of(new ColorizedString("", Color.white, Color.red));
+        changedLines = new BitSet(1);
     }
 
+    public List<ColorizedString> getLeft() {
+        return left;
+    }
+
+    public List<ColorizedString> getRight() {
+        return right;
+    }
+
+    public BitSet getChangedLines() {
+        return changedLines;
+    }
 }
